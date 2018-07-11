952e7ffa779187839e9ea5be433c80c3db4f56ec
Adam Horowitz <ahorowi4@gmail.com>
2018-07-11 15:06:00 -0400

    first draft of ItemList presentational component

---
+++ src/components/ItemList/index.js
@@ -0,0 +1,48 @@
+// @flow
+
+import * as React from "react";
+import { Table, Button, Glyphicon } from "react-bootstrap";
+
+type Item = {
+  num: number,
+  text: string,
+  type: "Multiple Choice" | "Dropdown" | "Text Box",
+  createdTimestamp: number
+};
+
+type Props = {
+  items: Array<Item>,
+  handleOnClick: (itemNum: number) => void,
+  handleOnDeleteClick: (itemNum: number) => void
+};
+
+const ItemList = (props: Props) => {
+  const { items, handleOnClick, handleOnDeleteClick } = props;
+
+  return (
+    <Table responsive hover>
+      <thead>
+        <tr>
+          <th> Number </th>
+          <th> Text </th>
+          <th> Type </th>
+          <th> Created Timestamp </th>
+          <th />
+        </tr>
+      </thead>
+      {items.map(item =>
+        <tr key={item.num}>
+          <td onClick={handleOnClick(item.num)}>item.num</td>
+          <td onClick={handleOnClick(item.num)}>item.text</td>
+          <td onClick={handleOnClick(item.num)}>item.type</td>
+          <td onClick={handleOnClick(item.num)}>item.createdTimestamp</td>
+          <td>
+            <Button onclick={handleOnDeleteClick(item.num)}>
+              <Glyphicon glyph="remove" />
+            </Button>
+          </td>
+        </tr>
+      )}
+    </Table>
+  );
+};
